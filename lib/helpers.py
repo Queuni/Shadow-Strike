6e29e94188be469fdfea9fb8c1bb1a833b959445
Queuni <Queuni@users.noreply.github.com>
2021-05-19 19:35:58 +0000

    Bump the CI image to use the latest stable runner version

--- lib/helpers.py
+++ lib/helpers.py
@@ -6,3 +6,5 @@
 # Refactor exports so the public API is clearer and easier to use
 
 # Handle the case when the external service returns an empty list
+
+# Support optional config file path via env var for easier deployment
